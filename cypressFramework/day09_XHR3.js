81fadfa3b2a8434ff4c70476bbab733cf1ed4ccc
Arslnmrv <arslanmerve1619@gmail.com>
2022-01-18 00:35:50 +0300

    mr/vx

--- cypressFramework/day09_XHR3.js
+++ cypressFramework/day09_XHR3.js
@@ -6,13 +6,14 @@ describe('XHR - XML, HTTP, Request', () => {
         cy.intercept('GET', '**/comments/*').as('getComment')
         //networku dinle
         //as -> degisken atiyor. daha sonra @ ile kullamilir
-        
+
         cy.get('.network-btn').click()
 
         cy.wait('@getComment').its('response').then((response) => {
             console.log(response)
 
             assert.equal(response.statusCode, 200, 'Status Code Control')
+
         })
     })
 })
